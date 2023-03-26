3d83f92c2bc85f3389617199e7e68b869f2cb9e9
antejadric <ajadric@pmfst.hr>
2023-03-26 19:35:05 +0200

    Zadatak 3 vj3. pod a) i b)

--- arithm.py
+++ arithm.py
@@ -3,5 +3,22 @@
 #a)
 import math as m
 n=10
+l=[]
+d=[]
 for ele in range(n):
-    print(ele)
+    x=float(input('Unesi točku: '))
+    l.append(x)
+    s=sum(l)
+    pros=s/n
+
+for ele in range(n):
+    a=(ele-pros)**2
+    d.append(a)
+e=sum(d)
+naz=n*(n-1)
+dev=m.sqrt(e/naz)
+print('Aritmetička sredina točaka {} iznosi {}. Standardna devijacija iznosi {}.'.format(l,pros,dev))
+
+
+
+
