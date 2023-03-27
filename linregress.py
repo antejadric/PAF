479c993dccdf4e1c8c2df692143657abc0c510cc
student <antejadric>
2023-03-27 09:56:02 +0200

    vjezba 3 zadatak 4

---
+++ linregress.py
@@ -0,0 +1,4 @@
+#linregress
+M=[0.052, 0.124, 0.168, 0.236, 0.284, 0.336] #mjerna jedinica za moment sile Nm
+φ=[0.1745, 0.3491, 0.5236, 0.6981, 0.8727, 1.0472] # mjern jedinica za kut zakretanja rad
+print(φ)
