7e237ba58aeb852be278355d2a76d33f47d210e7
antejadric <ajadric@pmfst.hr>
2023-04-02 10:30:09 +0200

    nedovrseni zadatak.

--- Zadatak2_Vjezba4.py
+++ Zadatak2_Vjezba4.py
@@ -1,1 +1,13 @@
 #Zadataka 2, Vjezba 4
+import Particle as P
+import numpy as np
+import matplotlib.pyplot as plt
+g=9.81
+v0=float(input('Pocetna brzina: '))
+theta=float(input('kut u stupnjevima'))
+k=np.radians(theta)
+d=(v0**2*np.sin(2*k))/g
+print('Domet kod analitickog rjesenja iznosi {}.'.format(d))
+
+p=P.Particle(10, 60, 0, 0)
+
