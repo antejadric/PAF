9c22b4976be25c15f3b47c88f5f63a403c3ec148
student <antejadric>
2023-04-03 09:52:00 +0200

    Zadatak 2 Vj 4 (nedovrseno)

--- Zadatak2_Vjezba4.py
+++ Zadatak2_Vjezba4.py
@@ -2,12 +2,21 @@
 import Particle as P
 import numpy as np
 import matplotlib.pyplot as plt
-g=9.81
-v0=float(input('Pocetna brzina: '))
-theta=float(input('kut u stupnjevima'))
-k=np.radians(theta)
-d=(v0**2*np.sin(2*k))/g
-print('Domet kod analitickog rjesenja iznosi {}.'.format(d))
+#x(t)=x_0+vx*t
 
+ad=[]
+k=np.radians(60)
+for t in np.arange(0.01,0.2,0.001):
+    d=10*np.cos(k)*t
+    ad.append(d)
+print(ad)
+
+er=[]
 p=P.Particle(10, 60, 0, 0)
 
+for dt in np.arange(0.01,0.2,0.001):
+    n=p.range(dt)
+    gr=abs(n-d)/d
+    a=gr*100
+    er.append(a)
+print(er)
