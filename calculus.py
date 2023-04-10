5d8d8b3903c98f3e97917aff872670d5dadcdcfa
antejadric <ajadric@pmfst.hr>
2023-04-10 12:51:58 +0200

    modul za vjezbu 5

--- calculus.py
+++ calculus.py
@@ -1,52 +1,71 @@
 #calculus
 #1.Zadatak##################################################
 import numpy as np
-class calculus:
-    def __init__(self, a, b, x, dx, n):
+
+class derivacije:
+    def __init__(self, a, b, x, dx):
         self.a = a
         self.b = b
         self.x = x
         self.dx = dx
-        self.n=n
-
-    def derivacijaI(self, f, x, dx, IIPmeth=True):
+        
+    def derivacijaI(self, f, IIPmeth=True):#derivacija sa twopoint i three point metodom
+        '''
+        Ova funkcija iz klase derivacije racuna derivaciju pomocu ThreePointMetod kao zadanom metodom te sa TwoPointmethod kao izbornom 
+        metodom ako netko unese ista drugo osim True na mjestu atributa IIPmeth
+        '''
         if IIPmeth:
-            der = (f(x)-f(x-dx))/dx
+            der = (f(self.x)-f(self.x-self.dx))/self.dx
         else:
-            der = (f(x+dx)-f(x-dx))/(2*dx)
+            der = (f(self.x+self.dx)-f(self.x-self.dx))/(2*self.dx)
         return der
     
-    def derivacijaII(self, f, a, b, dx, IIPmeth=True):
+    def derivacijaII(self, f, IIPmeth=True):#derivacija sa granicama
+        '''
+        Ova funkcija iz klase derivacije racuna derivaciju uzimajuci u obzir gornju i donju granicu derivacije kao i 
+        funkciju te tocku u kojoj ce derivacija biti izvrsena. Funkcija vraca listu tocaka u kojima ce biti izvrsena numericka derivacija
+        '''
         tocke_derivacije=[]
-        N=np.linspace(a,b,100)
+        N=np.linspace(self.a,self.b,100)
         for ele in N:
             if IIPmeth:
-                der=(f(ele)-f(ele-dx))/dx
+                der=(f(ele)-f(ele-self.dx))/self.dx
                 tocke_derivacije.append(der)
             else:
-                der=(f(ele+dx)-f(ele-dx))/(2*dx)
+                der=(f(ele+self.dx)-f(ele-self.dx))/(2*self.dx)
                 tocke_derivacije.append(der)
         return N,tocke_derivacije
-#2.Zadatak################################################
-    def integracijaI(self, f, x, a, b, dx, n):
-        gornja_gran=[]
-        donja_gran=[]
-        N=np.linspace(a,b,n)
-        for i in range(1,n):
-            F_U=f(N[i])*dx
+#2.Zadatak##########################################################
+class integracije:
+    def __init__(self,a,b,n):
+        self.a = a
+        self.b = b
+        self.n = n
+        self.dx = (b - a) / n
+
+    def integracijaI(self, f):#integracija za gornji i donji integral
+        '''
+        Ova funkcija iz klase integracije racuna gornji i donji integral. Tocnije, gornju i donju granicu.
+        '''
+        gornja_gran = []
+        donja_gran = []
+        dx = np.linspace(self.a, self.b, self.n+1)
+        N = len(dx)-1
+        for i in range(N):
+            F_U = f(dx[i+1]) * self.dx
             gornja_gran.append(F_U)
-            F_L=f(N[i-1])*dx
+            F_L = f(dx[i]) * self.dx
             donja_gran.append(F_L)
-        return gornja_gran,donja_gran 
+        return sum(gornja_gran), sum(donja_gran)
     
-    def integracijaII(self, f, a, b, dx, n):
-        numericka_vrijednost=[]
-        suma=[]
-        N=np.linspace(a,b,n)
-        for j in range(1,n):
-            sc=f(N[j]+(f(N[j])+f(N[0]))/2)
-            suma.append(sc)
-            h=sum(suma)
-            trap=dx*h
-            numericka_vrijednost.append(trap)
-        return numericka_vrijednost
+    def integracijaII(self, f):#integracija pmocu formule za trapezni integral
+        '''
+        Ova funkcija iz klase integracije racuna numericki rezultat pomocu trapezne integracije.
+        '''
+        num=[]
+        dx=np.linspace(self.a, self.b, self.n+1)
+        N=len(dx)-1
+        for j in range(N):
+            trap=(f(dx[j])+f(dx[j+1]))*abs(self.dx)/2
+            num.append(trap)
+        return sum(num)
