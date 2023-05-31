f09b5a8ef1d8886ee9f3e3a709e2b1d10fb93f39
antejadric <ajadric@pmfst.hr>
2023-05-31 13:40:02 +0200

    Modul za seminarski zadatak

--- numrac_vxt.py
+++ numrac_vxt.py
@@ -8,7 +8,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Num_racunanje:
-    def __init__(self, x, v, m, dt, t):
+    def __init__(self, x, v, m, dt, t, f):
+        self.f=f
         self.m = m
         self.dt = dt
         self.t = t
